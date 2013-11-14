18f0687a21e75b19eff39ccedd5c951f718c13c3
Drew Sorrels <amsorrels@gmail.com>
2013-11-14 17:35:53 -0500

    Cell complete

--- src/com/example/marblemaze/Cell.java
+++ src/com/example/marblemaze/Cell.java
@@ -2,5 +2,83 @@ package com.example.marblemaze;
 
 public class Cell
 {
+    private boolean[] walls;
+    private int       x;
+    private int       y;
 
+
+    public Cell()
+    {
+        this(0, 0);
+    }
+
+
+    public Cell(int x, int y)
+    {
+        walls = new boolean[4];
+        this.x = x;
+        this.y = y;
+        for (int i = 0; i < walls.length; i++)
+        {
+            walls[i] = true;
+        }
+
+    }
+
+
+    public int getNumWalls()
+    {
+        int count = 0;
+        for (int i = 0; i < walls.length; i++)
+        {
+            if (walls[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public int getRandomWallIndex()
+    {
+        int randIndex =
+            (int)Math.floor((Math.random() * (walls.length - 1)) + 0.5);
+        while (!walls[randIndex])
+        {
+            randIndex =
+                (int)Math.floor((Math.random() * (walls.length - 1)) + 0.5);
+        }
+        return randIndex;
+    }
+
+
+    public void setWall(int index, boolean val)
+    {
+        walls[index] = val;
+    }
+
+
+    public Cell east()
+    {
+        return new Cell(x + 1, y);
+    }
+
+
+    public Cell south()
+    {
+        return new Cell(x, y + 1);
+    }
+
+
+    public Cell west()
+    {
+        return new Cell(x - 1, y);
+    }
+
+
+    public Cell north()
+    {
+        return new Cell(x, y - 1);
+    }
 }
