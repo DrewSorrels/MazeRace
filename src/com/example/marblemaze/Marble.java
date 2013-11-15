cbce4ecf39f2faf4b53bf0a57905fe7fc55ca1be
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2013-11-14 20:44:22 -0500

    made marble maze screen with a ball on it that moves really slowly

---
+++ src/com/example/marblemaze/Marble.java
@@ -0,0 +1,39 @@
+package com.example.marblemaze;
+
+import sofia.graphics.ShapeMotion;
+import sofia.graphics.Color;
+import sofia.graphics.OvalShape;
+
+// -------------------------------------------------------------------------
+/**
+ *  This marble is an oval shape that rolls around on the phone screen.
+ *
+ *  @author Dennis Lysenko (dlysenko)
+ *  @version 2013.11.14
+ */
+
+public class Marble
+    extends OvalShape
+{
+    private static final float DENSITY = 1.0f;
+    private static final float RADIUS = 1.0f;
+
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new Marble object at the specified location.
+     * @param x the starting x-location
+     * @param y the starting y-location
+     */
+    public Marble(float x, float y)
+    {
+        super(x, y, Marble.RADIUS);
+        this.setDensity(Marble.DENSITY);
+        this.setFillColor(Color.gray);
+        this.setColor(Color.black);
+        this.setFriction(2.0f);
+        this.setShapeMotion(ShapeMotion.DYNAMIC); // dynamic = respond to force
+        this.setRestitution(1.0f); // "bouncy-ness"
+    }
+
+}
---
+++ src/com/example/marblemaze/MarbleMazeScreen.java
@@ -0,0 +1,33 @@
+package com.example.marblemaze;
+
+import sofia.app.ShapeScreen;
+import sofia.graphics.Color;
+
+// -------------------------------------------------------------------------
+/**
+ * A Controller for the marble maze game's main screen.
+ *
+ * @author Dennis Lysenko (dlysenko)
+ * @version Nov 14, 2013
+ */
+
+public class MarbleMazeScreen
+    extends ShapeScreen
+{
+    private Marble squishy;
+
+
+    @Override
+    public void initialize()
+    {
+        getCoordinateSystem().width(50);
+
+        // Apply standard Earth gravity to our scene.
+        setGravity(0, 0);
+
+        squishy = new Marble(15, 15);
+        add(squishy);
+        squishy.applyLinearImpulse(1, -1);
+    }
+
+}
