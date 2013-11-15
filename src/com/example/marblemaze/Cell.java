f5d5fa79aa667c9f5dcf6a0aea0b2f94347f35d0
Drew Sorrels <amsorrels@gmail.com>
2013-11-14 22:28:47 -0500

    cellllllll

--- src/com/example/marblemaze/Cell.java
+++ src/com/example/marblemaze/Cell.java
@@ -1,6 +1,5 @@
 package com.example.marblemaze;
 
-import com.example.marblemaze.util.WallOrientation;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
@@ -43,15 +42,21 @@ public class Cell
         }
         return count;
     }
-    public ArrayList<Integer> getWalls() {
+
+
+    public ArrayList<Integer> getWalls()
+    {
         ArrayList<Integer> ilWalls = new ArrayList<Integer>();
-        for(int i = 0; i < walls.length; i++) {
-            if(walls[i]) {
+        for (int i = 0; i < walls.length; i++)
+        {
+            if (walls[i])
+            {
                 ilWalls.add(i);
             }
         }
         return ilWalls;
     }
+
 
     public int getRandomWallIndex()
     {
@@ -64,9 +69,13 @@ public class Cell
         }
         return randIndex;
     }
-    public int oppositeWall(int wallIndex) {
+
+
+    public int oppositeWall(int wallIndex)
+    {
         return (wallIndex + 2) % walls.length;
     }
+
 
     public void setWall(int index, boolean val)
     {
@@ -95,5 +104,17 @@ public class Cell
     public Cell north()
     {
         return new Cell(x, y - 1);
+    }
+
+
+    public int getX()
+    {
+        return x;
+    }
+
+
+    public int getY()
+    {
+        return y;
     }
 }
