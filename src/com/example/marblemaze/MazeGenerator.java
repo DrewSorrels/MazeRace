4c87e1f177a8b87bc2a50e636fc27106bdc8c2bf
Drew Sorrels <amsorrels@gmail.com>
2013-11-15 19:55:20 -0500

    updating mazeGen

--- src/com/example/marblemaze/MazeGenerator.java
+++ src/com/example/marblemaze/MazeGenerator.java
@@ -1,5 +1,6 @@
 package com.example.marblemaze;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class MazeGenerator
@@ -12,7 +13,13 @@ public class MazeGenerator
     public MazeGenerator()
     {
         maze = new Maze(10);
-// cellWalls.add(maze.); //All walls around startPoint
+     cellWalls.add(maze.getAdjacentWalls(maze.getCell(0,0))); //All walls around startPoint
+    }
+
+
+    public Maze getMaze()
+    {
+        return maze;
     }
 
 
@@ -22,26 +29,35 @@ public class MazeGenerator
         {
             int randWall =
                 (int)Math.floor(Math.random() * (cellWalls.size() - 1) + 0.5);
-// List<Cell> cAdjCells = new ArrayList<Cell>();
-// cAdjCells.add(maze.getAdjadcentCells(cellWalls.get(randWall)));
-            // if(!cell.containsAll(cAdjCells)) //if the maze cell opposite the
-// wall
-// is not a part of the maze yet.
-// {
-// Cell cCurrent = cells.contains(cAdjCells.get(0) ? cAdjCells.get(0) :
-// cAdjCells.get(1);
-// Cell cOpposite = cells.contains(cAdjCells.get(0) ? cAdjCells.get(1) :
-// cAdjCells.get(0);
-// cells.add(cOpposite); //add the opposite cell
-// int wallPos = detWalls(cCurrent, cOpposite, cellWalls.get(randWall));
-// cOpposite.removeWall(cCurent.oppositeWall(wallPos));
-// cCurrent.removeWall(wallPos); //Remove the wall at the position of randWall
-// and set wall to false in each cell
-// cellWalls.add(maze.getAdjacentWalls(cOpposite)) //add all walls around that
-// } else {
-// cellWalls.remove(cellWalls.get(randWall));
-// }
-// adjCells.clear();
+            List<Cell> cAdjCells = new ArrayList<Cell>();
+            cAdjCells.add(maze.getAdjacentCells(cellWalls.get(randWall)));
+            if (!cells.containsAll(cAdjCells)) // if the maze cell opposite the
+                                                // wall
+                                                // is not a part of the maze yet.
+            {
+                Cell cCurrent =
+                    cells.contains(cAdjCells.get(0))
+                        ? cAdjCells.get(0)
+                        : cAdjCells.get(1);
+                Cell cOpposite =
+                    cells.contains(cAdjCells.get(0))
+                        ? cAdjCells.get(1)
+                        : cAdjCells.get(0);
+                cells.add(cOpposite); // add the opposite cell
+                int wallPos =
+                    detWalls(cCurrent, cOpposite, cellWalls.get(randWall));
+                cOpposite.setWall(cCurrent.oppositeWall(wallPos), false);
+                cCurrent.setWall(wallPos, false); // Remove the wall at the
+                                                    // position of randWall
+                                                    // and set wall to false in each cell
+                cellWalls.add(maze.getAdjacentWalls(cOpposite)); // add all
+                                                                // walls around that
+            }
+            else
+            {
+                cellWalls.remove(cellWalls.get(randWall));
+            }
+            cAdjCells.clear();
         }
     }
 
@@ -71,12 +87,6 @@ public class MazeGenerator
             }
         }
 
-    }
-
-
-    public Maze getMaze()
-    {
-        return maze;
     }
 
 }
