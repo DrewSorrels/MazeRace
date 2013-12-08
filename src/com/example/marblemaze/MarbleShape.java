6569ccb105b1cc7077bc2b30c05f1f33a04d27c6
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2013-12-07 21:14:56 -0500

    making marbleshape and wall stuff

--- src/com/example/marblemaze/MarbleShape.java
+++ src/com/example/marblemaze/MarbleShape.java
@@ -1,29 +1,28 @@
 package com.example.marblemaze;
 
-import java.util.Observable;
-import java.util.Observer;
 import com.example.marblemaze.observableevents.MarbleRemovedEvent;
 import com.example.marblemaze.observableevents.ObservableMazeComponent;
+import java.util.Observer;
 import sofia.graphics.Color;
 import sofia.graphics.OvalShape;
 import sofia.graphics.ShapeMotion;
 
 // -------------------------------------------------------------------------
 /**
- *  This marble is an oval shape that rolls around on the phone screen.
+ * This marble is an oval shape that rolls around on the phone screen.
  *
- *  @author Dennis Lysenko (dlysenko)
- *  @author Nicholas Kilmer (nkilmer8)
- *  @version 2013.12.06
+ * @author Dennis Lysenko (dlysenko)
+ * @author Nicholas Kilmer (nkilmer8)
+ * @version 2013.12.06
  */
 
 public class MarbleShape
     extends OvalShape
 {
-    private static final float DENSITY = 10f;
-    private static final float RADIUS = 0.3f;
-    private static final float FRICTION = 0.4f;
-    private static final float RESTITUTION = 0.6f;
+    private static final float      DENSITY     = 0f;
+    private static final float      RADIUS      = 0.3f;
+    private static final float      FRICTION    = 0f;
+    private static final float      RESTITUTION = 0.6f;
 
     private ObservableMazeComponent observable;
 
@@ -31,8 +30,11 @@ public class MarbleShape
     // ----------------------------------------------------------
     /**
      * Create a new Marble object at the specified location.
-     * @param x the starting x-location
-     * @param y the starting y-location
+     *
+     * @param x
+     *            the starting x-location
+     * @param y
+     *            the starting y-location
      */
     public MarbleShape(float x, float y)
     {
@@ -48,6 +50,7 @@ public class MarbleShape
         this.setRestitution(MarbleShape.RESTITUTION); // "bouncy-ness"
     }
 
+
     // ----------------------------------------------------------
     /**
      * sets the gravity of the marble
@@ -57,17 +60,35 @@ public class MarbleShape
         this.setGravityScale(0);
     }
 
+
     @Override
-    public void remove() {
+    public void remove()
+    {
         super.remove();
         notifyObservers(new MarbleRemovedEvent(this));
     }
 
+
+    // ----------------------------------------------------------
+    /**
+     * Adds an observer that would like to be notified of changes to this wall.
+     *
+     * @param obs
+     *            the observer in question
+     */
     public void addObserver(Observer obs)
     {
         observable.addObserver(obs);
     }
 
+
+    // ----------------------------------------------------------
+    /**
+     * Notifies all of the given observers that a change has been made.
+     *
+     * @param arg
+     *            information about the change that was made
+     */
     public void notifyObservers(Object arg)
     {
         observable.notifyObservers(arg);
