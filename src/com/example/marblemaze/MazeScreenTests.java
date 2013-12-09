afd3f4505af95b2c6c4b8d5341d5f923bdeb8df1
Drew <amsorrels@gmail.com>
2013-12-08 23:35:50 -0500

    stuff

--- src/com/example/marblemaze/MazeScreenTests.java
+++ src/com/example/marblemaze/MazeScreenTests.java
@@ -1,6 +1,16 @@
 package com.example.marblemaze;
 
 import android.content.Intent;
+import android.graphics.RectF;
+import com.example.marblemaze.observableevents.BulletAddedEvent;
+import com.example.marblemaze.observableevents.BulletRemovedEvent;
+import com.example.marblemaze.observableevents.HoleAddedEvent;
+import com.example.marblemaze.observableevents.ObservableMazeComponent;
+import com.example.marblemaze.observableevents.WallAddedEvent;
+import com.example.marblemaze.observableevents.WallRemovedEvent;
+import com.example.marblemaze.observableevents.WeaponSpawnerAddedEvent;
+import com.example.marblemaze.weapons.Laser;
+import com.example.marblemaze.weapons.LaserSpawner;
 
 // -------------------------------------------------------------------------
 /**
@@ -29,6 +39,7 @@ public class MazeScreenTests
         i.putExtra("holes", true);
     }
 
+
     /**
      * Sets up the test.
      */
@@ -56,5 +67,31 @@ public class MazeScreenTests
 
         assertNotNull(getScreen());
         assertNotNull(intent);
+    }
+
+
+    /**
+     * Asserts that you can move the marble on the screen.
+     */
+    public void testScreenWorks1()
+    {
+        Intent intent = new Intent(getScreen(), MazeScreen.class);
+        getScreen().startActivity(intent);
+        assertNotNull(getScreen());
+
+        ObservableMazeComponent obs = new ObservableMazeComponent();
+
+        getScreen().update(obs, new WallRemovedEvent(new Wall(0, 0, false)));
+        getScreen().update(obs, new WallAddedEvent(new Wall(0, 0, false)));
+        getScreen().update(obs, new BulletAddedEvent(new Laser(0, 0, 0)));
+        getScreen().update(obs, new BulletRemovedEvent(new Laser(0, 0, 0)));
+        getScreen().update(
+            obs,
+            new HoleAddedEvent(new Hole(new RectF(0, 0, 0, 0))));
+        getScreen().update(
+            obs,
+            new WeaponSpawnerAddedEvent(new LaserSpawner(0, 0, 3000, 0)));
+        getScreen().onTouchDown(25, 0);
+
     }
 }
