ded14740ec747a97c860a5a8e5a81272c03754f2
Drew <amsorrels@gmail.com>
2013-12-08 19:52:56 -0500

    laser and rocket tests

---
+++ src/com/example/marblemaze/weapons/LaserTests.java
@@ -0,0 +1,66 @@
+package com.example.marblemaze.weapons;
+
+import com.example.marblemaze.Maze;
+import com.example.marblemaze.Wall;
+import com.example.marblemaze.MarbleShape;
+import android.graphics.PointF;
+import student.TestCase;
+
+// -------------------------------------------------------------------------
+/**
+ * Tests the (@link Laser) class.
+ *
+ * @author Drew Sorrels (amsorr)
+ * @version 2013.12.08
+ */
+public class LaserTests
+    extends TestCase
+{
+    private Laser las;
+
+
+    /**
+     * Sets up tests.
+     */
+    public void setUp()
+    {
+        las = new Laser(0, 0, 0);
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Tests move method.
+     */
+    public void testMove()
+    {
+        las.move(12, 3);
+        assertEquals(new PointF(0, -3), las.getLinearVelocity());
+        las = new Laser(0, 0, 1);
+        las.move(1, 1);
+        las = new Laser(0, 0, 2);
+        las.move(1, 1);
+        las = new Laser(0, 0, 3);
+        las.move(1, 21);
+        las = new Laser(0, 0, 4);
+        las.move(1, 12);
+    }
+
+
+    /**
+     * Tests methods in Laser.
+     */
+    public void testLaser()
+    {
+        las.onCollisionWith(new MarbleShape(1, 1));
+        las.onCollisionWith(new Wall());
+
+        las.addObserver(new Maze(0, 0));
+        las.notify();
+
+        assertEquals(las, las.getShape());
+        las.remove();
+
+    }
+
+}
---
+++ src/com/example/marblemaze/weapons/RocketTests.java
@@ -0,0 +1,66 @@
+package com.example.marblemaze.weapons;
+
+import android.graphics.PointF;
+import com.example.marblemaze.MarbleShape;
+import com.example.marblemaze.Maze;
+import com.example.marblemaze.Wall;
+import student.TestCase;
+
+// -------------------------------------------------------------------------
+/**
+ * Test the (@link Rocket) class.
+ *
+ * @author Drew Sorrels (amsorr)
+ * @version 2013.12.08
+ */
+public class RocketTests
+    extends TestCase
+{
+    private Rocket rock;
+
+
+    /**
+     * Sets up all the tests.
+     */
+    public void setUp()
+    {
+        rock = new Rocket(0, 0, 0);
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Tests move method.
+     */
+    public void testMove()
+    {
+        rock.move(12, 3);
+        assertEquals(new PointF(0, -3), rock.getLinearVelocity());
+        rock = new Rocket(0, 0, 1);
+        rock.move(1, 1);
+        rock = new Rocket(0, 0, 2);
+        rock.move(1, 1);
+        rock = new Rocket(0, 0, 3);
+        rock.move(1, 21);
+        rock = new Rocket(0, 0, 4);
+        rock.move(1, 12);
+    }
+
+
+    /**
+     * Tests methods in Rocket.
+     */
+    public void testLaser()
+    {
+        rock.onCollisionWith(new MarbleShape(1, 1));
+        rock.onCollisionWith(new Wall());
+
+        rock.addObserver(new Maze(0, 0));
+        rock.notify();
+
+        assertEquals(rock, rock.getShape());
+        rock.remove();
+
+    }
+
+}
